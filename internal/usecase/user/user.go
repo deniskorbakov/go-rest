ecbc04bb33e27beb72f363bdae62e07950162c9e
Denis <korbakovd@gmail.com>
2025-12-23 15:40:17 +0300

    feat: add logic in methods

--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -1,6 +1,12 @@
 package user
 
-import "github.com/go-list-templ/grpc/internal/repo"
+import (
+	"context"
+
+	"github.com/go-list-templ/grpc/internal/domain/entity"
+	"github.com/go-list-templ/grpc/internal/domain/vo"
+	"github.com/go-list-templ/grpc/internal/repo"
+)
 
 type UseCase struct {
 	repo repo.UserPersistentRepo
@@ -8,4 +14,44 @@ type UseCase struct {
 
 func New(repo repo.UserPersistentRepo) *UseCase {
 	return &UseCase{repo: repo}
+}
+
+func (u *UseCase) All(ctx context.Context) ([]entity.User, error) {
+	users, err := u.repo.All(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (u *UseCase) Create(ctx context.Context, user entity.User) (entity.User, error) {
+	err := u.repo.Store(ctx, user)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
+}
+
+func (u *UseCase) Delete(ctx context.Context, userId vo.ID) error {
+	return u.repo.Destroy(ctx, userId)
+}
+
+func (u *UseCase) Show(ctx context.Context, userId vo.ID) (entity.User, error) {
+	user, err := u.repo.GetById(ctx, userId)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
+}
+
+func (u *UseCase) Update(ctx context.Context, user entity.User) (entity.User, error) {
+	updatedUser, err := u.repo.Change(ctx, user)
+	if err != nil {
+		return user, err
+	}
+
+	return updatedUser, nil
 }
