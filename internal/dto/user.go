71750b43608d307a864117704abc48252e60ebb3
Denis <korbakovd@gmail.com>
2025-12-23 21:20:08 +0300

    feat: add dto

---
+++ internal/dto/user.go
@@ -0,0 +1,32 @@
+package dto
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type CreateUserInput struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type UpdateUserInput struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type DeleteUserInput struct {
+	ID uuid.UUID `json:"id"`
+}
+
+type ShowUserInput struct {
+	ID uuid.UUID `json:"id"`
+}
+
+type UserOutput struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
