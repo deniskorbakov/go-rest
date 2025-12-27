d7e376bbf00b9d462f2cdaeb3755c80ff9f3d155
Denis <korbakovd@gmail.com>
2025-12-27 22:15:17 +0300

    feat: update order include pkgs

--- internal/controller/grpc/router.go
+++ internal/controller/grpc/router.go
@@ -2,10 +2,9 @@ package grpc
 
 import (
 	v1 "github.com/go-list-templ/grpc/internal/controller/grpc/v1"
-	pbgrpc "google.golang.org/grpc"
-
 	"github.com/go-list-templ/grpc/internal/usecase"
 	"go.uber.org/zap"
+	pbgrpc "google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
